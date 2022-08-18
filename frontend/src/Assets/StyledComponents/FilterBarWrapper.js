f65df3e7cc191501d3ce2973423ca7dfbfbc70c7
Dwayne Man <dwayne.man@gmail.com>
2022-08-19 02:03:49 +0800

    Removed height on FilterBarWrapper.

--- frontend/src/Assets/StyledComponents/FilterBarWrapper.js
+++ frontend/src/Assets/StyledComponents/FilterBarWrapper.js
@@ -3,7 +3,6 @@ import styled from "styled-components"
 const FilterBarWrapper = styled.section`
     display: flex;
     justify-content: center;
-    height: 80px;
     padding: 0 20px;
     margin: -50px 0px 80px;
 `
